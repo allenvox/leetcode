0dc85252d0ae100826310f9afe8e38938e25b331
allenvox <allenvox@users.noreply.github.com>
2023-05-26 20:40:53 +0700

    Add main function to task to show results

--- twosum.c
+++ twosum.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     int *result = malloc(sizeof(int) * 2);
     *returnSize = 2;
@@ -11,4 +14,12 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
         }
     }
     return result;
+}
+
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9, returnSize = 0;
+    int *twosum = twoSum(nums, 4, target, &returnSize);
+    printf("%d %d\n", twosum[0], twosum[1]);
+    return 0;
 }
