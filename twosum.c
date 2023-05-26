fd6eaa1e2485cee512a49607729d48e7c5d2a094
allenvox <allenvox@users.noreply.github.com>
2023-05-26 20:19:55 +0700

    Add 'Two Sum' task solution

---
+++ twosum.c
@@ -0,0 +1,14 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int *result = malloc(sizeof(int) * 2);
+    *returnSize = 2;
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target && i != j) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    return result;
+}
