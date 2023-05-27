84e541c3662584314340df22ade0fd4203646239
allenvox <allenvox@users.noreply.github.com>
2023-05-27 10:13:26 +0700

    Modify bash script to compile all tasks in the folder

--- buildrun.sh
+++ buildrun.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-if [ -z "$1" ]
-  then
-    echo "Usage: ./$(basename $0) <taskname>"
+
+if [ -z "$1" ]; then
+  for i in *.c; do
+    gcc -Wall -Wextra -o ${i%.c}.out "$i"
+    echo -e "\n${i%.c}:"
+    ./${i%.c}.out
+  done
+else
+  if test -f "$1.c"; then
+    gcc -Wall -Wextra -o $1.out $1.c
+    ./$1.out
   else
-    gcc -Wall -Wextra -o $1 $1.c
-    ./$1
+    echo -e "File '$1.c' doesn't exist.\nUsage: ./$(basename $0) <taskname>"
+  fi
 fi
