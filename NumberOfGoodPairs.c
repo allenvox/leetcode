1993ce4cca857d98d6db38551ab11910f23ebd0f
allenvox <allenvox@users.noreply.github.com>
2023-06-12 18:03:23 +0700

    Add solution for task 1512. Number of Good Pairs

---
+++ NumberOfGoodPairs.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int numIdenticalPairs(int *nums, int numsSize) {
+    int result = 0;
+    for(int j = 0; j < numsSize; j++) {
+        for(int i = 0; i < numsSize; i++) {
+            if(i >= j || nums[i] != nums[j]) continue;
+            result += 1;
+        }
+    }
+    return result;
+}
+
+void output(int *nums, int numsSize) {
+    printf("In array { ");
+    for(int i = 0; i < numsSize; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("} are %d good pairs\n", numIdenticalPairs(nums, numsSize));
+}
+
+int main() {
+    int nums1[6] = {1, 2, 3, 1, 1, 3};
+    output(nums1, 6);
+    int nums2[4] = {1, 1, 1, 1};
+    output(nums2, 4);
+    int nums3[3] = {1, 2, 3};
+    output(nums3, 3);
+    return 0;
+}
