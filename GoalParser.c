44dac869455349ee6709ed2cc4e2df501b9bf738
allenvox <allenvox@users.noreply.github.com>
2023-06-14 01:07:28 +0700

    Add solution for task 1678. Goal Parser Interpretation

---
+++ GoalParser.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *interpret(char *command) {
+    char *buff = (char *)calloc(strlen(command) + 1, 1);
+    if(!buff) {
+        return NULL;
+    }
+    int curchar = 0;
+    for(int i = 0; command[i] != '\0'; i++) {
+        if(command[i] == 'G') {
+            buff[curchar] = 'G';
+            curchar++;
+        } else if(command[i] == '(') {
+            if(command[i + 1] == ')') {
+                buff[curchar] = 'o';
+                curchar++;
+            } else {
+                buff[curchar] = 'a';
+                buff[curchar + 1] = 'l';
+                curchar += 2;
+            }
+        }
+    }
+    return buff;
+}
+
+void output(char *command) {
+    printf("%s\t--(parser)->\t%s\n", command, interpret(command));
+}
+
+int main() {
+    output("G()(al)");
+    output("G()()()()(al)");
+    output("(al)G(al)()()G");
+    return 0;
+}
