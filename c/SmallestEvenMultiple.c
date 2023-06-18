107d2ae1b500a287dba87f7f3926869103d6c6a2
allenvox <allenvox@users.noreply.github.com>
2023-06-18 16:14:14 +0700

    Add solution for task 2413. Smallest Even Multiple

---
+++ c/SmallestEvenMultiple.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int smallestEvenMultiple(int n) {
+    if(n % 2 == 0) return n;
+    return 2 * n;
+}
+
+void output(int n) {
+    printf("Smallest multiple of both 2 and %d is %d\n", n, smallestEvenMultiple(n));
+}
+
+int main() {
+    output(5);
+    output(6);
+    return 0;
+}
