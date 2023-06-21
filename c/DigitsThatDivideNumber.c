5b573a5c039b13b6f4c8b6e2e73fa38b83aa6879
allenvox <allenvox@users.noreply.github.com>
2023-06-21 12:08:09 +0700

    Add solution for task 2520. Count the Digits That Divide a Number

---
+++ c/DigitsThatDivideNumber.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int countDigits(int num) {
+    int copy = num, count = 0;
+    for(int i = 0; copy != 0; i++) {
+        int digit = copy % 10;
+        copy /= 10;
+        if(num % digit == 0) count++;
+    }
+    return count;
+}
+
+void output(int num) {
+    printf("In num %d there are %d digits that divide it\n", num, countDigits(num));
+}
+
+int main() {
+    output(7);
+    output(121);
+    output(1248);
+    return 0;
+}
