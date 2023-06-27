c7a92d7466f3e5d1f064a6f3e0a007f30501f9c1
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-06-27 22:25:24 +0700

    Add solution for task 231. Power Of Two

---
+++ c/PowerOfTwo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPowerOfTwo(int n) {
+    if(n > 0 && (n & n - 1) == 0) return true;
+    return false;
+}
+
+void output(int n) {
+    printf("%d is ", n);
+    if(!isPowerOfTwo(n)) printf("not ");
+    printf("a power of 2\n");
+}
+
+int main() {
+    output(1);
+    output(256);
+    output(7);
+    return 0;
+}
