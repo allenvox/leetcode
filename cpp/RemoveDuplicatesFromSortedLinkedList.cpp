e3fe8b6a24640bcbfa5accb056c3412afa7fe45d
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-07-02 16:51:16 +0600

    Add solution for task 83. Remove Duplicates from Sorted Linked List

---
+++ cpp/RemoveDuplicatesFromSortedLinkedList.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode *current = head;
+        while(current && current->next) {
+            if(current->val == current->next->val) current->next = current->next->next;
+            else current = current->next;
+        }
+        return head;
+    }
+
+    void output(ListNode *head) {
+        ListNode *temp = head;
+        cout << "Sorted linked list:\n";
+        while(temp != nullptr) {
+            cout << temp->val << " -> ";
+            temp = temp->next;
+        }
+        cout << "nullptr\n";
+        temp = deleteDuplicates(head);
+        cout << "Without duplicates:\n";
+        while(temp != nullptr) {
+            cout << temp->val << " -> ";
+            temp = temp->next;
+        }
+        cout << "nullptr\n\n";
+    }
+};
+
+int main() {
+    Solution s;
+    ListNode *l1 = new ListNode(1, new ListNode(1, new ListNode(2)));
+    s.output(l1);
+    ListNode *l2 = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
+    s.output(l2);
+    return 0;
+}
