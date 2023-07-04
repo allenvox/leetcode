6f292c7fb6cb454ad3c84ce8d3230920b71e9a12
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-07-04 12:30:48 +0600

    Modify flags in buildrun script

--- buildrun.sh
+++ buildrun.sh
@@ -4,10 +4,10 @@ if [ -z "$1" ]; then
   echo -e "Usage: ./$(basename $0) <taskname>"
 else
   if test -f "$1.c"; then
-    gcc -Wall -Wextra -Werror -o $1.cout $1.c
+    gcc -Wall -Wextra -Werror -Wshadow -Wsign-conversion -o $1.cout $1.c
     ./$1.cout
   elif test -f "$1.cpp"; then
-    g++ -Wall -Wextra -Werror -pedantic -std=c++1y -o $1.cppout $1.cpp
+    g++ -Wall -Wextra -Werror -Wshadow -Wsign-conversion -pedantic -std=c++17 -o $1.cppout $1.cpp
     ./$1.cppout
   else
     echo -e "File '$1.c' doesn't exist.\nUsage: ./$(basename $0) <taskname>"
