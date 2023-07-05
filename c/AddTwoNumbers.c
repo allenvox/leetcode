fcb0e4e77e7aa3d5a20d35dd7a0fe548f9cb62b1
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-07-05 09:17:07 +0600

    Make solution for AddTwoNumbers readable

--- c/AddTwoNumbers.c
+++ c/AddTwoNumbers.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct ListNode {
   int val;
   struct ListNode *next;
 };
+
+void deleteList(struct ListNode *l) {
+  if(l->next != NULL)
+    deleteList(l->next);
+  free(l);
+}
 
 struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
   struct ListNode *res = NULL, *cur, *prev = NULL;
@@ -33,13 +40,44 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
   return res;
 }
 
+void output(struct ListNode *l1, struct ListNode *l2) {
+  struct ListNode *temp = l1, *res = addTwoNumbers(l1, l2);
+  char s[64] = "\n";
+  while(temp) {
+    char buff[5];
+    sprintf(buff, "%d ", temp->val);
+    strcat(s, buff);
+    temp = temp->next;
+  }
+  strcat(s, "+ ");
+  temp = l2;
+  while(temp) {
+    char buff[5];
+    sprintf(buff, "%d ", temp->val);
+    strcat(s, buff);
+    temp = temp->next;
+  }
+  strcat(s, "= ");
+  temp = res;
+  while(temp) {
+    char buff[5];
+    sprintf(buff, "%d ", temp->val);
+    strcat(s, buff);
+    temp = temp->next;
+  }
+  for(int i = strlen(s); i >= 0; i--) {
+    printf("%c", s[i]);
+  }
+  deleteList(res);
+}
+
 int main() {
   struct ListNode *l1 = malloc(sizeof(struct ListNode));
   l1->val = 2;
   l1->next = malloc(sizeof(struct ListNode));
   l1->next->val = 4;
   l1->next->next = malloc(sizeof(struct ListNode));
-  l1->next->next->val = 3;
+  l1->next->next->val = 4;
   l1->next->next->next = NULL;
 
   struct ListNode *l2 = malloc(sizeof(struct ListNode));
@@ -50,11 +88,8 @@ int main() {
   l2->next->next->val = 4;
   l2->next->next->next = NULL;
 
-  struct ListNode *res = addTwoNumbers(l1, l2);
-  while (res) {
-    printf("%d ", res->val);
-    res = res->next;
-  }
-  printf("\n");
+  output(l1, l2);
+  deleteList(l1);
+  deleteList(l2);
   return 0;
 }
