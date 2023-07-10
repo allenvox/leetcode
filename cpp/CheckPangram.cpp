ae483c91453bdff4e29020596313433f02a9d21a
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-07-10 10:39:25 +0600

    Format code

--- cpp/CheckPangram.cpp
+++ cpp/CheckPangram.cpp
@@ -3,29 +3,31 @@
 using namespace std;
 
 class Solution {
-public:
-    bool checkIfPangram(string sentence) {
-        vector<int> letters(26);
-        for(unsigned long i = 0; i < sentence.size(); i++) {
-            letters[(unsigned long)(sentence[i] - 'a')] = 1;
-        }
-        for(unsigned long i = 0; i < 26; i++) {
-            if(letters[i] == 0) return false;
-        }
-        return true;
+ public:
+  bool checkIfPangram(string sentence) {
+    vector<int> letters(26);
+    for (unsigned long i = 0; i < sentence.size(); i++) {
+      letters[(unsigned long)(sentence[i] - 'a')] = 1;
     }
+    for (unsigned long i = 0; i < 26; i++) {
+      if (letters[i] == 0) return false;
+    }
+    return true;
+  }
 
-    void output(string sentence) {
-        cout << "String \"" << sentence << "\" is ";
-        if(!checkIfPangram(sentence)) cout << "not ";
-        cout << "a pangram" << endl;
-    }
+  void output(string sentence) {
+    cout << "String \"" << sentence << "\" is ";
+    if (!checkIfPangram(sentence)) cout << "not ";
+    cout << "a pangram" << endl;
+  }
 };
 
 int main() {
-    Solution s;
-    cout << "Pangram is a sentence that contains every letter of alphabet at least once" << endl;
-    s.output("thequickbrownfoxjumpsoverthelazydog");
-    s.output("leetcode");
-    return 0;
+  Solution s;
+  cout << "Pangram is a sentence that contains every letter of alphabet at "
+          "least once"
+       << endl;
+  s.output("thequickbrownfoxjumpsoverthelazydog");
+  s.output("leetcode");
+  return 0;
 }
