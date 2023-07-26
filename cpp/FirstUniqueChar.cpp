e3543c05d02522975706049bf7f2709b4743be09
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-07-26 19:05:40 +0600

    Add solution for task 387. First Unique Character in a String

---
+++ cpp/FirstUniqueChar.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+ public:
+  int firstUniqChar(string s) {
+    unordered_map<char, int> counter(0);
+    for (char c : s) counter[c]++;
+    for (unsigned long i = 0; i < s.size(); i++) {
+      if (counter[s[i]] == 1) return i;
+    }
+    return -1;
+  }
+
+  void output(string s) {
+    int res = firstUniqChar(s);
+    cout << "First unique character in string \"" << s << "\" ";
+    if (res != -1)
+      cout << "is on index " << res << " (\'" << s[(unsigned long)res] << "\')";
+    else
+      cout << "doesn't exist";
+    cout << endl;
+  }
+};
+
+int main() {
+  Solution s;
+  s.output("leetcode");
+  s.output("love leetcode");
+  s.output("aabb");
+  return 0;
+}
