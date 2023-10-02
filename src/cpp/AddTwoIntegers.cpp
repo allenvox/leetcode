04fb1b00ebedf70e3a2f77e796a62ff049e6e44b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-10-02 22:16:45 +0700

    Reimplement AddTwoIntegers task in C++

---
+++ src/cpp/AddTwoIntegers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Solution {
+ public:
+  static int sum(int num1, int num2) { return num1 + num2; }
+
+  static void output(int num1, int num2) {
+    std::cout << num1 << " + " << num2 << " = " << sum(num1, num2)
+              << '\n';
+  }
+};
+
+int main() {
+  Solution::output(2, 3);
+  Solution::output(-2, -8);
+  Solution::output(0, 0);
+  return 0;
+}
